4817a487387120c8e2b0963e1e980dd28530743d
Ajinthan Selvaratnam <ajinthan@semantica.nl>
2021-04-13 16:08:36 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="dashboard">
+      <WidgetContentSmall slot="widget-content" :model="widgetContent" />
+      <WidgetContentSmall slot="widget-content" :model="widgetContent" />
+      <WidgetContentSmall slot="widget-content" :model="widgetContent" />
+      <WidgetContentSmall slot="widget-content" :model="widgetContent" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WidgetContentSmall from './components/WidgetContentSmall.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WidgetContentSmall
+  },
+  data: () => ({
+    widgetContent: {
+      primaryContent: "122 hours",
+      primaryContentTitle: "Total hours billed this year",
+      secondaryContent: "-5,3%",
+      secondarySubContent: "10,4%"
+    }
+  })
 }
 </script>
 
@@ -24,5 +37,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.dashboard {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-template-rows: 140px 140px 140px 140px;
+  gap: 10px;
 }
 </style>
